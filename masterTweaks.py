5be0342acab4034c8097135bb8076fcd6263956b
Jake Drake <tacodrake95@yahoo.com>
2017-01-16 22:20:02 -0800

    meh

---
+++ masterTweaks.py
@@ -0,0 +1,10 @@
+class main():
+    def __init__(self, b):
+        self.newMasters = []
+        self.b = b
+        self.b.master.extend(self.newMasters)
+
+    def unload(self):
+        for key in self.newMasters:
+            self.b.master.remove(key)
+        return True
